ed9761752b3408d911aa410934c056768b83300d
butterstulle93 <daniel-co@live.de>
2021-09-30 19:07:04 +0200

    dd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,17 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Cheat Sheet")
+
+st.sidebar.title("Versuch")
+
+
+topic = st.sidebar.selectbox(
+    "Unter Thema",
+    ("Github", "Latex", "Pandas", "Python")
+)
+
+if topic == "Github":
+    sub_topic = st.sidebar.selectbox(
+    "Thema",
+    ("A", "B", "C", "D")
+)
